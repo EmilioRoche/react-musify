93ce90cfdf6965fbe968757de6467d52ab2b08f3
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-06-13 16:14:32 -0300

    got the rails backend to work with the react front end with routing

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
 import {BrowserRouter as Router,  Routes,  Route, Link} from "react-router-dom"
-
-const client_id = process.env.REACT_APP_CLIENT_KEY;
-
-const AUTH_URL = `http://localhost:3000/api/login`;
+//change to actual page later
+const AUTH_URL = `http://localhost:3000/api/v1/login`;
 /*const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}
 &response_type=code
 &redirect_uri=https://emilioroche.github.io/react-musify/
@@ -17,14 +15,14 @@ export default function Login() {
     className="d-flex justify-content-center align-items-center"
     style={{ minHeight: `100vh`}}
     >
-        <Link to="/musify/login">
+        <Link to="/api/v1/login">
         <a className="btn btn-success btn-lg" href={AUTH_URL}>
             login with Spotify
         </a>
         </Link>
     </Container>
         <Routes>
-            <Route exact path="/musify/login"></Route>
+            <Route exact path="/api/v1/login"></Route>
         </Routes>
     </Router> 
     );
