279493d4be773ee62a0888b0ac6fe96f22659c52
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-06-13 14:34:12 -0300

    naming import v2

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
-import BrowserRouter from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom"
 
 const client_id = process.env.REACT_APP_CLIENT_KEY;
 
